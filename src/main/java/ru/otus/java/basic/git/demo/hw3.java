363f6536a1ae6b22a4cdaf3da675717eb2fbe6cd
TatRaznitsyna <traznitsyna@gmail.com>
2024-10-09 20:32:52 +0500

    коммит новый класс

---
+++ src/main/java/ru/otus/java/basic/git/demo/hw3.java
@@ -0,0 +1,10 @@
+package ru.otus.java.basic.git.demo;
+
+public class hw3 {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
