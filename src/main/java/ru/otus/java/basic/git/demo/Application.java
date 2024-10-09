6a137b77c36a938c5da74b90dcbb173ee852b1f3
TatRaznitsyna <traznitsyna@gmail.com>
2024-10-09 20:26:21 +0500

    первый коммит

---
+++ src/main/java/ru/otus/java/basic/git/demo/Application.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.git.demo;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
